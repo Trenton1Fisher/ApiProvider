05fd08bc6a84049fe1059f8baa57a9d456fa2b5b
Trenton1Fisher <trenton0fisher@gmail.com>
2024-11-17 08:15:50 +0000

    setting up db

---
+++ db/schema.sql
@@ -0,0 +1,37 @@
+-- Should be able to adapt this to any one table csv file to work with the program
+CREATE TABLE dogs (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    origin VARCHAR(100),
+    type VARCHAR(50),
+    unique_feature TEXT,
+    friendly_rating SMALLINT CHECK (friendly_rating BETWEEN 1 AND 10),
+    life_span SMALLINT CHECK (life_span > 0),
+    size VARCHAR(50),
+    grooming_needs VARCHAR(100),
+    exercise_requirements NUMERIC(3,1) CHECK (exercise_requirements >= 0),
+    good_with_children BOOLEAN,
+    intelligence_rating SMALLINT CHECK (intelligence_rating BETWEEN 1 AND 10),
+    shedding_level VARCHAR(50),
+    health_issues_risk VARCHAR(50),
+    average_weight NUMERIC(5,2),
+    training_difficulty SMALLINT CHECK (training_difficulty BETWEEN 1 AND 10)
+);
+
+-- Due to read only nature of project we are gonna learn about indexing 
+-- Should have an initial performance cost i believe but should be worth it 
+CREATE INDEX idx_dogs_name ON dogs(name);
+CREATE INDEX idx_dogs_origin ON dogs(origin);
+CREATE INDEX idx_dogs_type ON dogs(type);
+CREATE INDEX idx_dogs_unique_feature ON dogs(unique_feature);
+CREATE INDEX idx_dogs_friendly_rating ON dogs(friendly_rating);
+CREATE INDEX idx_dogs_life_span ON dogs(life_span);
+CREATE INDEX idx_dogs_size ON dogs(size);
+CREATE INDEX idx_dogs_grooming_needs ON dogs(grooming_needs);
+CREATE INDEX idx_dogs_exercise_requirements ON dogs(exercise_requirements);
+CREATE INDEX idx_dogs_good_with_children ON dogs(good_with_children);
+CREATE INDEX idx_dogs_intelligence_rating ON dogs(intelligence_rating);
+CREATE INDEX idx_dogs_shedding_level ON dogs(shedding_level);
+CREATE INDEX idx_dogs_health_issues_risk ON dogs(health_issues_risk);
+CREATE INDEX idx_dogs_average_weight ON dogs(average_weight);
+CREATE INDEX idx_dogs_training_difficulty ON dogs(training_difficulty);
--- db/seed.go
+++ db/seed.go
@@ -1,1 +1,29 @@
-//Go file to load all data into the made postgres database
+package main
+
+import (
+	"fmt"
+	"bufio"
+)
+
+//Should be able to just change this to be compatable with other csv files without any relationships and just one table
+type Dog struct {
+	Name                string
+	Origin              string
+	Type                string
+	UniqueFeature       string
+	FriendlyRating      int8 
+	LifeSpan            int8 
+	Size                string
+	GroomingNeeds       string
+	ExerciseRequirements float32
+	GoodWithChildren    bool
+	IntelligenceRating  int8 
+	SheddingLevel       string
+	HealthIssuesRisk    string
+	AverageWeight       float32
+	TrainingDifficulty  int8 
+}
+
+func main() {
+	
+}
