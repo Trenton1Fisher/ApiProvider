2090512d0895a051a38325e6ec9d84a3dc1c6540
Trenton1Fisher <trenton0fisher@gmail.com>
2024-11-26 07:58:43 +0000

    added api url

--- web/app.js
+++ web/app.js
@@ -1,5 +1,16 @@
-const apiUrl = '%%PROD_API_URL%%';  // Placeholder
+const apiUrl = 'https://api.dog.trentonfisher.xyz';
 
-const tempButtonEvent = () => {
-    console.log("API URL:", apiUrl);
-}
+const tempButtonEvent = async () => {
+    try {
+        const res = await fetch(`${apiUrl}/getToken`)
+        if (res.ok) {
+            const data = await res.json();
+            console.log(data);
+        } else {
+            console.error('Failed to fetch:', res.statusText);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
